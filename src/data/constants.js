7b00216a2414b8a2b70296b3d8330736c3b31890
Shubham Kumar Sahoo <shubham.sahoo.1712@gmail.com>
2024-06-04 23:04:59 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,426 @@
+export const Bio = {
+  name: "Shubham Kumar Sahoo",
+  roles: [
+    "LLM Engineer",
+    "Data Scientist/Analyst",
+    "ML Engineer",
+    "Software Developer",
+    "Python Developer",
+    
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/DeathStar1711",
+  resume:
+    "https://drive.google.com/file/d/11Yvqg8HicvKI0cqCCByVFBhm83vzdTxD/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/shubham-sahoo-574a2b196/",
+  twitter: "https://x.com/shubham03460375",
+  insta: "https://www.instagram.com/shubham.19.xx/",
+  facebook: "https://m.facebook.com/shubham.sahoo.108",
+};
+
+export const skills = [
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "R",
+        image:
+          "https://www.r-project.org/logo/Rlogo.svg",
+      },
+      {
+        name: "C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg",
+      },
+      {
+        name: "C",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "Java",
+        image:
+          "https://www.vectorlogo.zone/logos/java/java-vertical.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "JQuery",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/f/fd/JQuery-Logo.svg",
+      },
+    ],
+  },
+  {
+    title: "Frameworks & Libraries",
+    skills: [
+      {
+        name: "Hugging Face",
+        image:
+          "https://huggingface.co/datasets/huggingface/brand-assets/resolve/main/hf-logo.svg",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Flask_logo.svg/1280px-Flask_logo.svg.png",
+      },
+      {
+        name: "Django",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlFFyY16N5NRhzoG24RqB7x8Ok2t8Bdgs-tuMsOBXS2Q&s",
+      },
+      {
+        name: "Tenserflow",
+        image:
+          "https://static-00.iconduck.com/assets.00/tensorflow-icon-1911x2048-1m2s54vn.png",
+      },
+      {
+        name: "Keras",
+        image:
+          "https://miro.medium.com/v2/resize:fit:600/1*DKu_54iqz6C-p6ndo7rO3g.png",
+      },
+      {
+        name: "Scikit Learn",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/2560px-Scikit_learn_logo_small.svg.png",
+      },
+      {
+        name: "Pandas",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/e/ed/Pandas_logo.svg",
+      },
+      {
+        name: "Numpy",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/31/NumPy_logo_2020.svg",
+      },
+      {
+        name: "BootStrap",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg",
+      },
+      {
+        name: "Streamlit",
+        image:
+          "https://streamlit.io/images/brand/streamlit-mark-color.png",
+      },
+      {
+        name: "Matplotlib",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/8/84/Matplotlib_icon.svg",
+      },
+    ],
+  },
+  {
+    title: "DevOps & Databases",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+      },
+      {
+        name: "Google Cloud",
+        image:
+          "https://static-00.iconduck.com/assets.00/google-cloud-platform-logo-icon-2048x1824-pg4wzspq.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "SQLite",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/38/SQLite370.svg",
+      },
+      {
+        name: "Oracle Database",
+        image:
+          "https://www.vectorlogo.zone/logos/oracle/oracle-icon.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Developer Tools",
+    skills: [
+      {
+        name: "Visual Studio Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/2/2d/Visual_Studio_Code_1.18_icon.svg",
+      },
+      {
+        name: "Pycharm",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/1d/PyCharm_Icon.svg",
+      },
+      {
+        name: "IntelliJ IDEA",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/9c/IntelliJ_IDEA_Icon.svg",
+      },
+      {
+        name: "Jupyter",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
+      },
+      {
+        name: "Google Colab",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d0/Google_Colaboratory_SVG_Logo.svg",
+      },
+      {
+        name: "AutoCAD",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/6/6e/AutoCad_new_logo.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://pbs.twimg.com/media/FQIGEpiaMAMJnMN.jpg:large",
+    role: "AI Intern",
+    company: "Personifwy",
+    date: "April 2022 - July 2022",
+    desc: "Developed advanced Celebrity Image Classifier using Python, Tensorflow, Keras, and Scikit-learn. Curated and preprocessed a diverse celebrity image dataset for model training. Achieved high accuracy through model optimization and rigorous evaluation. Collaborated with cross-functional teams and led documentation and knowledge-sharing efforts. Successfully deployed the classifier with positive stakeholder feedback, demonstrating adaptability in a remote work environment.",
+    skills: [
+      "TensorFlow",
+      "Matplotlib",
+      "Keras",
+      "Scikit-Learn",
+      "Convolutional Neural Networks (CNN)",
+      "Natural Language Processing (NLP)",
+    ],
+    doc: "https://drive.google.com/file/d/1JNIPR1rqyQyNUAF4-5jLZvNVMeDYXH_F/view?usp=sharing",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
+    school: "Kalinga Institute of Industrial Technology, Bhubaneswar",
+    date: "Sept 2020 - Aug 2024",
+    grade: "8.96 CGPA",
+    desc: "I have achieved a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have passed out with a CGPA of 8.96. I had taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I was also a member of the National Service Scheme (NSS) at KIIT, where I developed and honed my leadership qualities while leading my clan, successfully steering us to numerous accolades for our innovative and revolutionary ideas and executions.",
+    degree: "Bachelor of Technology - B.Tech, Computer Science and System Engineering",
+  },
+  {
+    id: 1,
+    img: "https://www.mericity.com/MeriCityMedia/Business/Logo/DelhiPublicSchool.jpg",
+    school: "Delhi Public School, Ahmedabad",
+    date: "Apr 2018 - Apr 2020",
+    grade: "85%",
+    desc: "I completed my class 12 high school education at DPS Bopal, Ahmedabad, where I studied Physics, Chemistry, Mathematics (PCM) with Computer Science.",
+    degree: "CBSE(XII), PCM with Computer Science",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuss-7D7Hycmy_2KwQfW0KcAEWP0-opbbVeg&s",
+    school: "DAV International School, Ahmedabad",
+    date: "Apr 2013 - Apr 2018",
+    grade: "81.6%",
+    desc: "I completed my class 10 education at DAV International School, Ahmedabad, where I studied Science with Computer Application.",
+    degree: "CBSE(X), Science with Computer Application",
+  },
+];
+
+export const projects = [
+  {
+    id: 30,
+    title: "Chat with PDF using Retrieval Augmented Generation (RAG)",
+    date: "Apr 2024 - May 2024",
+    description:
+      "The goal of this project is to create a user-centric and intelligent system that enhances information retrieval from PDF documents through natural language queries. The project focuses on streamlining the user experience by developing an intuitive interface, allowing users to interact with PDF content using language they are comfortable with. To achieve this, I leverage the Retrieval Augmented Generation (RAG) methodology introduced by Meta AI researchers.",
+    image:
+      "https://github.com/DeathStar1711/Chat-PDF/raw/master/Recording%202024-06-04%20205338.gif",
+    tags: [
+      "Python", 
+      "Streamlit",
+      "Hugging Face",
+      "Retrival Augmented Generation(RAG)",
+      "Langchain",
+      "PyPDF2",
+      "Groq API",
+      "FAISS",
+    ],
+    category: "machine learning",
+    github: "https://github.com/DeathStar1711/Chat-PDF",
+    webapp: "https://github.com/DeathStar1711/Chat-PDF",
+  },
+  {
+    id: 11,
+    title: "Plant Disease Classification",
+    date: "Jan 2024 - Mar 2024",
+    description:
+      "This project aims to develop a deep learning model for identifying different plant diseases based on image data. The model leverages transfer learning from the pre-trained EfficientNetB5 model, fine-tuning its final layers for the specific task of plant disease classification.",
+    image:
+      "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihYijbrs_t9LEVW2uS3rTqsOx1OgPS5K7PDOxF_ql014uqsfmnvFn7_qWsOvDvW8Gbef86oGjpoUiotcZ_Fd0v-_vwq9fT1YHfU=w2499-h1377-rw-v1",
+    tags:[
+      "Pandas",
+      "Numpy",
+      "OS",
+      "Matplotlib",
+      "Pillow (PIL)",
+      "Tensorflow",
+      "Keras"
+    ],
+    category: "machine learning",
+    github: "https://github.com/DeathStar1711/Plant-Disease-Classification",
+    webapp: "https://github.com/DeathStar1711/Plant-Disease-Classification",
+  },
+  {
+    id: 9,
+    title: "Online Shopping Website <Addidas> using Python-Django",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Addidas is an online shopping website developed using Python and the Django framework. It aims to provide users with a seamless online shopping experience, including browsing products, adding items to a cart, and managing orders.",
+    image:
+      "https://github.com/DeathStar1711/Online_Shopping_Website_using_Python-Django/raw/master/addidas%20(1)%20(3).gif",
+    tags: [
+      "Python", 
+      "Django", 
+      "HTML", 
+      "CSS", 
+      "Javascript", 
+      "JQuery", 
+      "Jinja", 
+      "SQLite"
+    ],
+    category: "python app",
+    github: "https://github.com/DeathStar1711/Online_Shopping_Website_using_Python-Django",
+    webapp: "https://github.com/DeathStar1711/Online_Shopping_Website_using_Python-Django",
+  },
+  {
+    id: 0,
+    title: "Analyzing and Visualizing the Space Race",
+    date: "Apr 2023 - May 2023",
+    description:
+      "In this project, we employed advanced data visualization techniques. We used multi-line charts with Matplotlib to compare multiple data series over time and ensured data integrity by removing NaN values and duplicates. Pie and donut charts illustrated categorical data distributions, while grouped bar charts and box plots with Plotly compared multiple categories and visualized data distributions, helping detect outliers and understand data spread. These methods enhanced our data interpretation and presentation.",
+    image:
+      "https://user-images.githubusercontent.com/98851253/174491084-c01fe84e-cfbc-4f42-8540-b9bf86e1623a.png",
+    tags: ["Python", "Matplotlib", "Plotly"],
+    category: "data science",
+    github: "https://github.com/DeathStar1711/Analyzing_and_Visualizing_the_Space_Race",
+    webapp: "https://github.com/DeathStar1711/Analyzing_and_Visualizing_the_Space_Race",
+  },
+  {
+    id: 3,
+    title: "Personal Blog Website using Python-Flask",
+    date: "Mar 2023 - Apr 2023",
+    description:
+      "In this Flask-based blog web app project, we implemented several key features. We enabled user registration and login, ensuring that only registered users can access certain functionalities. To enhance security, we protected specific routes by requiring login and, for administrative actions, admin privileges. We created relational databases to manage users, blog posts, and comments efficiently. Additionally, we allowed any authenticated user to add comments to blog posts, encouraging interaction and engagement within the platform.",
+    image:
+      "https://github.com/DeathStar1711/Personal_Blog_Website_using_Python-Flask/raw/main/Animation.gif",
+    tags: ["Python", "Flask", "SQLAlchemy", "Jinja", "HTML", "CSS", "Bootstrap"],
+    category: "python app",
+    github: "https://github.com/DeathStar1711/Personal_Blog_Website_using_Python-Flask",
+    webapp: "https://github.com/DeathStar1711/Personal_Blog_Website_using_Python-Flask",
+  },
+  {
+    id: 1,
+    title: "Predicting Earnings using Multivariable Regression",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "In this project, we utilized several advanced concepts. We filtered data on multiple conditions to ensure precise analysis. For visualization, we used Seaborn to create bubble charts and plot linear regressions, which helped in understanding relationships within the data. Additionally, we employed scikit-learn to run regression analyses, providing robust predictive insights. These techniques collectively enhanced our data analysis and presentation capabilities.",
+    image:
+      "https://user-images.githubusercontent.com/98851253/175329626-882a00f8-7ddd-4cec-aa75-428723329779.png",
+    tags: [
+      "Python", "Seaborn", "Scikit-learn", "Matplotlib",
+    ],
+    category: "data science",
+    github: "https://github.com/DeathStar1711/Predicting_Earnings_using_Multivariable_Regression",
+    webapp: "https://github.com/DeathStar1711/Predicting_Earnings_using_Multivariable_Regression",
+  },
+  {
+    id: 4,
+    title: "Workout Tracking Using Google Sheets",
+    date: "Oct 2023",
+    description:
+      "This Python app leverages Nutonix and Sheety APIs to track workouts. You can describe exercises and save them to a Google Sheet using natural language, all while keeping data secure with environment variables and basic authentication.",
+    image:
+      "https://user-images.githubusercontent.com/98851253/156811102-164c20f4-f722-46b6-9a2b-ccdb6eca519e.gif",
+    tags: ["Python", "Requests", "OS", "Nutonix API", "Sheety API"],
+    category: "python app",
+    github: "https://github.com/DeathStar1711/Workout_Tracking_App",
+    webapp: "https://github.com/DeathStar1711/Workout_Tracking_App",
+  },
+  {
+    id: 8,
+    title: "Face Recognition",
+    date: "Sept 2023",
+    description:
+      "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+    image:
+      "https://dontrepeatyourself.org/media/face-recognition-with-python-dlib-and-deep-learning_cezKZBj.png",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/DeathStar1711/Face-Recognition-AI-with-Python",
+    webapp: "https://github.com/DeathStar1711/Face-Recognition-AI-with-Python",
+  },
+  {
+    id: 2,
+    title: "Space Invaders",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "In this project, we focused on creating a 2D game using PyGame. We drew (blitted) items on the screen to build the game environment and organized our code with the use of classes for better structure and maintenance. Handling user input was essential for responding to the player's actions, and we implemented event loops to manage game events and updates efficiently. These concepts were crucial in developing an interactive and well-structured game.",
+    image:
+      "https://user-images.githubusercontent.com/98851253/173201632-b6fcf00f-53f7-4b64-aa65-c44b53800e39.gif",
+    tags: ["Python", "Pygame", "OS"],
+    category: "python app",
+    github: "https://github.com/DeathStar1711/Space_Invaders_Game",
+    webapp: "https://github.com/DeathStar1711/Space_Invaders_Game",
+  },
+  
+];
+
