4c9fe00a22d151e44e0c7c00c3d3a4db66daa938
DeathStar1711 <98171210+DeathStar1711@users.noreply.github.com>
2024-06-05 13:36:12 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,7 +12,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/DeathStar1711",
   resume:
-    "https://drive.google.com/file/d/11Yvqg8HicvKI0cqCCByVFBhm83vzdTxD/view?usp=sharing",
+    "https://drive.google.com/file/d/1QoCPJofKIq6p7duopaSoAcApdHyouwYl/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/shubham-sahoo-574a2b196/",
   twitter: "https://x.com/shubham03460375",
   insta: "https://www.instagram.com/shubham.19.xx/",
